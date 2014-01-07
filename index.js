d02a8c8f8070991d6d5af5f42841ca6632ec5fc3
Ramijack <sanjeevpande7@gmail.com>
2014-01-07 23:53:34 +0530

    Added each players click and basic animation on three balls

--- index.js
+++ index.js
@@ -7,10 +7,36 @@ $(document).ready(function(){
 	};
 
 	$('li').on('click', function(){
-		var ball = "<span class='ball'></span>"
-		$(this).append(ball);
+		
+		var ball;
+
+		if($(this).find('.ball').length === 1){
+			ball = "<span class='ball ball1'></span>"
+			$(this).find('div').append(ball);
+			$(this).find('.ball1').css('margin-left', '30px');
+		}
+		else if($(this).find('.ball').length === 2){
+			ball = "<span class='ball ball2'></span>"
+			$(this).find('div').append(ball);
+			$(this).find('.ball1').css('margin-left', '20px');
+			$(this).find('.ball2').css('margin-top', '25px');
+
+			$(this).find('div').addClass('rotateB');
+
+		}
+		else{
+			ball = "<div><span class='ball'></span></div>"
+			$(this).append(ball);	
+		}
+
+
+
+		
 		if(players["red"]){
 			$(this).find('.ball').addClass('red');
+
+
+
 			players["red"] = false;
 			players["green"] = true;
 			players["blue"] = false;
