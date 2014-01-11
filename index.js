f01a0a867eb567f44235bb24d4bb3d94e39f0e94
sanjeevpande <sanjeevpande7@gmail.com>
2014-01-11 22:08:26 +0530

    Chaining on corners and sides

--- index.js
+++ index.js
@@ -40,23 +40,7 @@ $(document).ready(function(){
 
 		var ball;
 
-		if(clickedBox.find('.ball').length === 1){
-			ball = "<span class='ball ball1'></span>";
-			clickedBox.find('div').append(ball);
-			//clickedBox.find('.ball1').css('margin-left', '30px');
-		}
-		else if(clickedBox.find('.ball').length === 2){
-			ball = "<span class='ball ball2'></span>";
-			clickedBox.find('div').append(ball);
-			clickedBox.find('.ball2').css('margin-left', '20px');
-			clickedBox.find('.ball2').css('margin-top', '25px');
-
-			clickedBox.find('div').addClass('rotateB');
-
-		}
-		else if(clickedBox.find('.ball').length === 3){
-			
-			var prevBox = clickedBox.prev('li'),
+		var prevBox = clickedBox.prev('li'),
 				nextBox = clickedBox.next('li'),
 				parentRow = clickedBox.parent('ul'),
 				prevParentRow = parentRow.prev(),
@@ -65,6 +49,82 @@ $(document).ready(function(){
 				prevParentBox = prevParentRow.find('li').eq(clickedBoxIndex),
 				nextParentBox = nextParentRow.find('li').eq(clickedBoxIndex),
 				currentPlayerColor = clickedBox.css('border-top-color');
+
+		if(clickedBox.find('.ball').length === 1){
+			
+			if(clickedBox[0] === $('ul:first').find('li:first')[0]
+				|| clickedBox[0] === $('ul:first').find('li:last')[0]
+				|| clickedBox[0] === $('ul:last').find('li:first')[0]
+				|| clickedBox[0] === $('ul:last').find('li:last')[0]){
+
+				
+				clickedBox.find('div').remove();
+
+
+				appendToClickedBox(clickedBox, prevBox, currentPlayerColor);
+				appendToClickedBox(clickedBox, nextBox, currentPlayerColor);
+				appendToClickedBox(clickedBox, prevParentBox, currentPlayerColor);
+				appendToClickedBox(clickedBox, nextParentBox, currentPlayerColor);
+
+
+				prevBox.find('span').css('background-color', currentPlayerColor);
+		
+				nextBox.find('span').css('background-color', currentPlayerColor);
+
+				prevParentBox.find('span').css('background-color', currentPlayerColor);
+		
+				nextParentBox.find('span').css('background-color', currentPlayerColor);
+
+				/*event.stopPropogation();*/
+			}
+			else{
+				
+				ball = "<span class='ball ball1'></span>";
+				clickedBox.find('div').append(ball);
+			}
+		}
+		else if(clickedBox.find('.ball').length === 2){
+
+			if(clickedBox[0] === $('ul:first').find('li').eq(clickedBox.index())[0]
+				|| clickedBox[0] === $('ul:last').find('li').eq(clickedBox.index())[0]
+				|| clickedBox[0] === $('ul').find('li:first').eq(clickedBox.parent().index())[0]
+				|| clickedBox[0] === $('ul').find('li:last').eq(clickedBox.parent().index())[0]){
+
+				
+				clickedBox.find('div').remove();
+
+
+				appendToClickedBox(clickedBox, prevBox, currentPlayerColor);
+				appendToClickedBox(clickedBox, nextBox, currentPlayerColor);
+				appendToClickedBox(clickedBox, prevParentBox, currentPlayerColor);
+				appendToClickedBox(clickedBox, nextParentBox, currentPlayerColor);
+
+
+				prevBox.find('span').css('background-color', currentPlayerColor);
+		
+				nextBox.find('span').css('background-color', currentPlayerColor);
+
+				prevParentBox.find('span').css('background-color', currentPlayerColor);
+		
+				nextParentBox.find('span').css('background-color', currentPlayerColor);
+
+				/*event.stopPropogation();*/
+			}
+			else{
+
+				ball = "<span class='ball ball2'></span>";
+				clickedBox.find('div').append(ball);
+				clickedBox.find('.ball2').css('margin-left', '20px');
+				clickedBox.find('.ball2').css('margin-top', '25px');
+
+				clickedBox.find('div').addClass('rotateB');
+
+			}
+
+		}
+		else if(clickedBox.find('.ball').length === 3){
+			
+			
 
 			
 			clickedBox.find('div').remove();
@@ -96,15 +156,51 @@ $(document).ready(function(){
 
 	appendToClickedBox = function(clickedBox, boxObject, currentPlayerColor){
 
+		if(!boxObject.length){
+			return;
+		}
+
 		if(boxObject.find('span').length){
 			if(boxObject.find('span').length === 1){
-				ball = "<span class='ball ball1'></span>";	
-				boxObject.find('span').parent().append(ball);
+
+				if(clickedBox[0] === $('ul:first').find('li:first')[0]
+					|| clickedBox[0] === $('ul:first').find('li:last')[0]
+					|| clickedBox[0] === $('ul:last').find('li:first')[0]
+					|| clickedBox[0] === $('ul:last').find('li:last')[0]){
+
+					boxObject.find('span').css('background-color', currentPlayerColor);
+					boxObject.trigger('myClick');
+				}
+				else{
+					ball = "<span class='ball ball1'></span>";	
+					boxObject.find('span').parent().append(ball);
+
+					if(boxObject.find('span').length > 1 && (boxObject[0] === $('ul:first').find('li:first')[0]
+						|| boxObject[0] === $('ul:first').find('li:last')[0]
+						|| boxObject[0] === $('ul:last').find('li:first')[0]
+						|| boxObject[0] === $('ul:last').find('li:last')[0])){
+
+						boxObject.find('span').css('background-color', currentPlayerColor);
+						boxObject.trigger('myClick');
+					}					
+				}
 			}
 			else if(boxObject.find('span').length === 2){
-				ball = "<span class='ball ball2'></span>";	
-				boxObject.find('div').addClass('rotateB');
-				boxObject.find('span').parent().append(ball);
+				
+				if(clickedBox[0] === $('ul:first').find('li').eq(clickedBox.index())[0]
+					|| clickedBox[0] === $('ul:last').find('li').eq(clickedBox.index())[0]
+					|| clickedBox[0] === $('ul').find('li:first').eq(clickedBox.parent().index())[0]
+					|| clickedBox[0] === $('ul').find('li:last').eq(clickedBox.parent().index())[0]){
+
+					boxObject.find('span').css('background-color', currentPlayerColor);
+					boxObject.trigger('myClick');
+
+				}
+				else{
+					ball = "<span class='ball ball2'></span>";	
+					boxObject.find('div').addClass('rotateB');
+					boxObject.find('span').parent().append(ball);
+				}
 			}
 			else if(boxObject.find('span').length === 3){
 				boxObject.find('span').css('background-color', currentPlayerColor);
